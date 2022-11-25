ad963babcdddb092f0d8a3c88e169a1272f7be00
zohar <zohar.bobrin@tictuk.com>
2022-11-26 01:08:49 +0200

    adding log

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 (async () => {
+    console.log('send request');
     const loginRes = await axios.post('http://api.cup2022.ir/api/v1/user/login', {
         email: 'test@test123.com',
         password: 'test123321'

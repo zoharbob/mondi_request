8ea4d9faf68c2684c547f5ba8e12cf7dbe80871e
zohar <zohar.bobrin@tictuk.com>
2022-11-26 01:24:39 +0200

    adding route

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+const express = require('express');
+const app = express();
 const axios = require('axios');
 
-(async () => {
+const port = process.env.PORT || 4000;
+
+app.get('/', async (req, res) => {
     console.log('send request');
     const loginRes = await axios.post('http://api.cup2022.ir/api/v1/user/login', {
         email: 'test@test123.com',
         password: 'test123321'
     })
 
-    console.log(loginRes);
-})()
+    console.log(loginRes.data);
+    res.send(loginRes.data);
+});
+
+app.listen(port, () => console.log('listening on port', port));

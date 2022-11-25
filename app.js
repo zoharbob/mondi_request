55c374400d69bdcd86b51347bb717d1e41eaf1ba
zohar <zohar.bobrin@tictuk.com>
2022-11-26 01:50:18 +0200

    adding routes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 4000;
 
-app.get('/', async (req, res) => {
-    console.log('send request');
+app.use(bodyParser.json());
+
+app.get('/login', async (req, res) => {
+    console.log('login request');
     const loginRes = await axios.post('http://api.cup2022.ir/api/v1/user/login', {
         email: 'test@test123.com',
         password: 'test123321'
@@ -14,5 +17,21 @@ app.get('/', async (req, res) => {
     console.log(loginRes.data);
     res.send(loginRes.data);
 });
+
+app.post('/game', async (req, res) => {
+    console.log('game request');
+    const reqBody = req.body;
+    const token = req.headers.token;
+
+    const gameRes = await axios.post('http://api.cup2022.ir/api/v1/bydate', reqBody, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    console.log(gameRes.data);
+    res.send(gameRes.data);
+});
+
 
 app.listen(port, () => console.log('listening on port', port));
